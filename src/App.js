3d989d75e54e7b322387471e55e14a5c61d30d8c
Stepan Chaban <aziibro@yandex.ru>
2023-07-24 18:14:56 +0400

    update draw

--- src/App.js
+++ src/App.js
@@ -5,20 +5,9 @@ import ScoreBoard from './components/ScoreBoard';
 import ResetButton from './components/ResetButton';
 
 function App() {
-  const WIN_CONDITIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  const [xPlaying, setXPlaying] = useState(true);
   const [board, setBoard] = useState(Array(9).fill(null));
-  const [scores, setScores] = useState({ xScore: 0, oScore: 0 });
+  const [xPlaying, setXPlaying] = useState(true);
+  const [scores, setScores] = useState({ xScore: 0, oScore: 0, drawScore: 0 });
   const [gameOver, setGameOver] = useState(false);
 
   const handleBoxClick = boxIdx => {
@@ -41,26 +30,47 @@ function App() {
         let { oScore } = scores;
         oScore += 1;
         setScores({ ...scores, oScore });
-      } else {
+      } else if (winner === 'X') {
         let { xScore } = scores;
         xScore += 1;
         setScores({ ...scores, xScore });
       }
     }
 
+    const emptyCells = updatedBoard.filter(cell => cell === null);
+    const isDraw = emptyCells.length === 0;
+
+    if (isDraw) {
+      let { drawScore } = scores;
+      drawScore += 1;
+      setScores({ ...scores, drawScore });
+    }
+
     // Change active player
     setXPlaying(!xPlaying);
   };
 
-  const checkWinner = board => {
-    for (let i = 0; i < WIN_CONDITIONS.length; i++) {
-      const [x, y, z] = WIN_CONDITIONS[i];
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
 
-      if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+  const checkWinner = cells => {
+    for (let i = 0; i < lines.length; i++) {
+      const [x, y, z] = lines[i];
+
+      if (cells[x] && cells[x] === cells[y] && cells[y] === cells[z]) {
         setGameOver(true);
-        return board[x];
+        return cells[x];
       }
     }
+    return null;
   };
 
   const resetBoard = () => {
--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,12 +1,13 @@
 import './ScoreBoard.css';
 
 const ScoreBoard = ({ scores, xPlaying }) => {
-  const { xScore, oScore } = scores;
+  const { xScore, oScore, drawScore } = scores;
   return (
     <div className="scoreboard">
       <span className={`score x-score ${!xPlaying && 'inactive'}`}>
         X - {xScore}
       </span>
+      <span className={`draw`}>draw - {drawScore}</span>
       <span className={`score o-score ${xPlaying && 'inactive'}`}>
         O - {oScore}
       </span>
